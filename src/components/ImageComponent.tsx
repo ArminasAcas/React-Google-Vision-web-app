1aa940978dcb1039919a7df089fc66aee3f82100
ArminasAcas <arminasprollas@gmail.com>
2023-10-03 23:53:22 +0300

    Added selected image display

--- src/components/ImageComponent.tsx
+++ src/components/ImageComponent.tsx
@@ -3,6 +3,9 @@ import "../css/ImageComponent.css"
 export default function Image(props: {imageURL: string}) {
 
     return(
-        <img src={props.imageURL} className="image"></img>
+        <>
+            {props.imageURL.length > 0 ? 
+            <img src={props.imageURL} className="image image--show"></img> : <div className="image image--empty"> </div>}
+        </> 
     )
 }
--- src/components/ImageRecognitionComponent.tsx
+++ src/components/ImageRecognitionComponent.tsx
@@ -15,8 +15,6 @@ export default function ImageRecognition() {
             <input className="image-form__input" type="file" accept="image/*" onChange={HandleChange}></input>
             <button >Atpažinti objektus</button>
         </form>
-
-        
         </>
     )
 }
