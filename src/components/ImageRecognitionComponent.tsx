e600dc0cbcf5dcc1060a2a35f575890ef9b0668e
ArminasAcas <arminasprollas@gmail.com>
2023-10-04 23:51:19 +0300

    Added state saving for image file

--- src/components/ImageRecognitionComponent.tsx
+++ src/components/ImageRecognitionComponent.tsx
@@ -3,17 +3,26 @@ import Image from "./ImageComponent";
 import "../css/ImageRecognitionComponent.css"
 
 export default function ImageRecognition() {
-    const [image, setImage] = useState(""); 
+    const [imageURL, setImageURL] = useState(""); 
+    const [ImageFile, setImageFile] = useState<File | null>(null);
 
     function HandleChange(e: React.ChangeEvent<HTMLInputElement>){
-        if(e.target.files) setImage(URL.createObjectURL(e.target.files[0]));
+        if(e.target.files)
+        {
+            setImageURL(URL.createObjectURL(e.target.files[0]));
+            setImageFile(e.target.files[0]);
+        } 
+    }
+
+    function handleClick(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+        e.preventDefault();
     }
     return(
         <>
         <form className="image-form">
-            <Image imageURL={image}></Image>
+            <Image imageURL={imageURL}></Image>
             <input className="image-form__input" type="file" accept="image/*" onChange={HandleChange}></input>
-            <button className="image-form__button">Atpažinti objektus</button>
+            <button className="image-form__button" onClick={handleClick}>Atpažinti objektus</button>
         </form>
         </>
     )
